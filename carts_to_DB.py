39b6aa1f17d9844e204c24876f814d445026b269
AntonyLeo <91914154+AntonyLeo@users.noreply.github.com>
2023-11-20 18:13:24 -0800

    Update carts_to_DB.py

--- carts_to_DB.py
+++ carts_to_DB.py
@@ -1,16 +1,9 @@
 import pandas as pd
-import os
 from sqlalchemy import create_engine
 
-folder_path = "C:\Users\maste\Desktop\Project\Carts"
+csv_file_path = 'path/to/your/csvfile.csv'
 
-csv_files = [os.path.join(folder_path, f) for f in os.listdir(folder_path) if f.endswith('.csv')]
-
-all_data = pd.DataFrame()
-for file in csv_files:
-    df = pd.read_csv(file)
-    df.insert(1, 'Date', os.path.basename(file))  # Insert 'Date' as the second column
-    all_data = all_data.append(df, ignore_index=True)
+df = pd.read_csv(csv_file_path)
 
 # MySQL connection details
 db_endpoint = 'yielddatabase.coz72rpcgefb.us-east-1.rds.amazonaws.com'
@@ -22,4 +15,4 @@ db_name = 'harvest'
 connection_string = f"mysql+mysqlconnector://{db_username}:{db_password}@{db_endpoint}/{db_name}"
 engine = create_engine(connection_string)
 
-all_data.to_sql('carrito', engine, if_exists='replace', index=False)
+df.to_sql('carrito', engine, if_exists='replace', index=False)
